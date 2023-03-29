d955a406b2e109e7131ca3e7a998d71fcc0cd7a2
FractalRose <brandon.jy.lam@gmail.com>
2023-03-29 19:59:20 +1100

    Bongo Cat/screen font change/keymap & colemak

--- app/src/display/status_screen.c
+++ app/src/display/status_screen.c
@@ -70,12 +70,12 @@ lv_obj_t *zmk_display_status_screen() {
                                      LV_LABEL_PART_MAIN, LV_STATE_DEFAULT,
                                      lv_theme_get_font_small());
     lv_obj_align(zmk_widget_layer_status_obj(&layer_status_widget), NULL, LV_ALIGN_IN_BOTTOM_LEFT,
-                 0, 0);
+                 0, -23);
 #endif
 
 #if IS_ENABLED(CONFIG_ZMK_WIDGET_BONGO_CAT)
     zmk_widget_bongo_cat_init(&bongo_cat_widget, screen);
-    lv_obj_align(zmk_widget_bongo_cat_obj(&bongo_cat_widget), NULL, LV_ALIGN_CENTER, 0, 0);
+    lv_obj_align(zmk_widget_bongo_cat_obj(&bongo_cat_widget), NULL, LV_ALIGN_IN_BOTTOM_RIGHT, 25, 0);
 #endif
 
 #if IS_ENABLED(CONFIG_ZMK_WIDGET_WPM_STATUS)
